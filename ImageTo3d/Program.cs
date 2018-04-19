9d55208bb9dd4976bc5ffcc58e837e28a009258f
NikolasUntoten <nikolasr@ggaub.com>
2018-04-18 17:14:24 -0700

    Initial commit

---
+++ ImageTo3d/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageTo3d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
